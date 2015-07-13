f92e95c8a7f9b6e9415b46b2aae9ea2c910152bd
kirankotari <kotarikirankumar@gmail.com>
2015-07-13 23:41:04 +0530

    Default Projects
    Wavemaker default Projects (Employee Profile, Dashboard)

---
+++ Dashboard_Defalt/src/main/webapp/pages/Main/Main.js
@@ -0,0 +1,11 @@
+Application.$controller("MainPageController", ["$scope", function ($scope) {
+    "use strict";
+}]);
+
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
